fd3389b700fc77250ca5eb777b1086c6350438a1
thisiskhandev <hassan.khan.dev@proton.me>
2024-01-18 23:32:57 +0500

    First intiallized

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Faq } from "./components/Faq";
+
+export const App = () => {
+  const [number, setNumber] = useState(0);
+  return (
+    <>
+      <section>
+        <div
+          className="flex justify-center align-middle items-center flex-col gap-2 m-auto"
+          style={{ height: "100vh", maxWidth: 600 }}
+        >
+          <Button onClick={() => setNumber(number + 1)}>Incremental</Button>
+          <h2>
+            Number: <span>{number}</span>
+          </h2>
+          <Faq />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default App;
